c83b076d7c07338cee8070f10b475ded558473f3
Milza <marco.milza@capgemini.com>
2023-01-27 17:03:40 +0100

    exercise done

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-// const records: Customers = [
-//    { id: 10, name: 'Luca',     city: 'Milan',    country: 'Italy'  },
-//    { id: 20, name: 'Mario',    city: 'Rome',     country: 'Italy'  },
-//    { id: 30, name: 'Anna',     city: 'New York', country: 'USA'    },
-//    { id: 40, name: 'Stefano',  city: 'Boston',   country: 'USA'    },
-//    { id: 50, name: 'Gloria',   city: 'Paris',    country: 'France' },
-//    { id: 60, name: 'Giuseppe', city: 'Rome',     country: 'Italy'  },
-//  ] 
-var clients = [
-    { id: 10, name: 'Luca', city: 'Milan', country: 'Italy' },
-    { id: 20, name: 'Mario', city: 'Rome', country: 'Italy' },
-    { id: 30, name: 'Anna', city: 'New York', country: 'USA' },
-    { id: 40, name: 'Stefano', city: 'Boston', country: 'USA' },
-    { id: 50, name: 'Gloria', city: 'Paris', country: 'France' },
-    { id: 60, name: 'Giuseppe', city: 'Rome', country: 'Italy' },
-];
-var groupData = function (customers, field) {
-    var obj = {};
-    customers.forEach(function (e) { return obj[e[field]] = []; });
-    customers.forEach(function (e) { return obj[e[field]].push(e.id); });
-    return obj;
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
-console.log(groupData(clients, 'country'));
-console.log(groupData(clients, 'city'));
+const a = ['bau', 9];
+const myObj = {
+    key1: 1,
+    key2: 2,
+};
+const myConst = 'key1';
+const freeNpropObj2 = {
+    ciao: 'buz',
+    miao: 'qwerty',
+    bau: 'fizz',
+};
+const freeNpropObj3 = {
+    ciao: 'buz',
+    miao: 'qwerty',
+    bau: 'fizz',
+};
+const holdOn = () => setTimeout(() => null, 2000);
+const p = new Promise((res, rej) => {
+    holdOn();
+    res('done');
+});
+const doAfterProm = () => __awaiter(void 0, void 0, void 0, function* () {
+    const res = yield p;
+    console.log(res);
+    console.log('promise awaited');
+});
+doAfterProm();
+p.then(r => console.log('this is what i log after the promise'));
+console.log('here what to log sync');
--- index.ts
+++ index.ts
@@ -1,60 +1,55 @@
-// const records: Customers = [
-//    { id: 10, name: 'Luca',     city: 'Milan',    country: 'Italy'  },
-//    { id: 20, name: 'Mario',    city: 'Rome',     country: 'Italy'  },
-//    { id: 30, name: 'Anna',     city: 'New York', country: 'USA'    },
-//    { id: 40, name: 'Stefano',  city: 'Boston',   country: 'USA'    },
-//    { id: 50, name: 'Gloria',   city: 'Paris',    country: 'France' },
-//    { id: 60, name: 'Giuseppe', city: 'Rome',     country: 'Italy'  },
-//  ] 
- 
-//  create a function that group the data on a specified field. 
- 
-//  # Example 1: groupCustomersBy(records, 'country') result = 
-//  {
-//     'Italy':  [ 10, 20, 60 ],
-//     'USA':    [ 30, 40 ],
-//     'France': [ 50 ],
-//  }
- 
-//   # Example 2: groupCustomersBy(records, 'city') result = {
-//     'Milan':    [ 10 ],
-//     'Rome':     [ 20, 60 ],
-//     'New York': [ 30 ],
-//     'Boston':   [ 40 ],
-//     'Paris':    [ 50 ]
-//  } 
 
-//  TODO: create the groupCustomersBy function as requested and model/type all the elements
-//  in typescript (the records, the function params/result type)
- 
-type Customer =   {
-  id: number, 
-  name: string,
-  city: string,    
-  country: string 
+
+const a: readonly [string, number]= ['bau', 9] 
+
+type Obj = {
+  key1: number;
+  key2:  number;
 }
 
-type Customers =  Customer[]
-
-type Res =   {
- [key: string]: number[];
+const myObj : Obj = {
+  key1: 1,
+  key2: 2,
 }
 
-const data : Customers = [
- { id: 10, name: 'Luca',     city: 'Milan',    country: 'Italy'  },
- { id: 20, name: 'Mario',    city: 'Rome',     country: 'Italy'  },
- { id: 30, name: 'Anna',     city: 'New York', country: 'USA'    },
- { id: 40, name: 'Stefano',  city: 'Boston',   country: 'USA'    },
- { id: 50, name: 'Gloria',   city: 'Paris',    country: 'France' },
- { id: 60, name: 'Giuseppe', city: 'Rome',     country: 'Italy'  },
-] 
+const myConst : keyof Obj = 'key1'
 
-const group = (customers: Customers, field: keyof Customer) : Res =>  {
-  const obj = {} 
-  customers.forEach(e => obj[e[field]] = [] )
-  customers.forEach(e => obj[e[field]].push(e.id) )
-  return obj
-} 
+// 2 sintaxes, same result
+type myObj2 = {
+   [key: string]: string
+}
 
-console.log(group(data, 'country'))
-console.log(group(data, 'city'))
+type myObj3 = Record<string, string>
+
+const freeNpropObj2 : myObj2 = {
+  ciao: 'buz',
+  miao: 'qwerty',
+  bau: 'fizz',
+}
+
+
+const freeNpropObj3 : myObj3 = {
+  ciao: 'buz',
+  miao: 'qwerty',
+  bau: 'fizz',
+}
+
+const holdOn = () => setTimeout(() => null, 2000)
+
+const p = new Promise((res, rej) => {
+  holdOn();
+  res('done')
+}) 
+
+const doAfterProm = async () => {
+  const res = await p
+  console.log(res)
+  console.log('promise awaited')
+}
+
+doAfterProm()
+
+p.then(r => console.log('this is what i log after the promise'))
+
+console.log('here what to log sync')
+
